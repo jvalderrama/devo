aa1fa337274fbf9471398b1fa8f1c691cb39c3e9
Jorge Valderrama <jvalderrama@beesion.com>
2020-09-30 12:35:48 +0200

    Pipe

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import unittest
-import app
+from app.app import app
 
 class TestHello(unittest.TestCase):
 
     def setUp(self):
-        app.app.testing = True
-        self.app = app.app.test_client()
+        app.testing = True
+        self.app = app.test_client()
 
     def test_check_devo_ok(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'OK\n')
+        self.assertEqual(rv.data, b'KO\n')
 
     def test_check_devo_ko(self):
         rv = self.app.get('/')
